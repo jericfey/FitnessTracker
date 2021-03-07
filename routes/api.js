68e9e7aa8d757b72f6256d8569dc973ad53d0e4a
Jeri Fey <jericfey@gmail.com>
2021-03-07 15:55:55 -0700

    Put working to database

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
 
+//Validate responses before sending
+const goodResponse = (dbWorkout, res) => {
+  res.json(dbWorkout);
+};
+
+const badResponse = (err, res) => {
+  res.status(400).json(err);
+};
+
 const findAll = (req, res) => {
   Workout.find({})
     .then((data) => res.json(data))
@@ -8,6 +17,12 @@ const findAll = (req, res) => {
       console.log("error", err);
       res.json(err);
     });
+};
+
+const create = ({ body }, res) => {
+  Workout.create({})
+    .then((data) => goodResponse(data, res))
+    .catch((err) => badResponse(err, res));
 };
 
 //* add routes for ../public api.js
@@ -73,6 +88,6 @@ const findAll = (req, res) => {
 //     });
 // });
 
-router.route("/workouts").get(findAll);
+router.route("/workouts").get(findAll).post(create);
 
 module.exports = router;
